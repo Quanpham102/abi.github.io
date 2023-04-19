39d5ef22b13fa26bff808c9ef97c730196eb6568
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-04-19 20:20:25 +0700

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,12 +1,15 @@
-        var message = "I love you";
+        var message = "I Love You";
         var index = 0;
-        
         function typeMessage() {
           if (index < message.length) {
             document.getElementById("message").innerHTML += message.charAt(index);
             index++;
-            setTimeout(typeMessage, 100);
+            setTimeout(typeMessage, 202);
+          } 
+          else {
+            index = 0;
+            document.getElementById("message").innerHTML = "";
+            setTimeout(typeMessage, 520);
           }
         }
-        
         typeMessage();
