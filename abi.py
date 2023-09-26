7d69abdf5f96940bc5eb9fb6890dbb3e6aee8576
abiq02 <quanabi@debian.com>
2023-09-26 10:05:09 -0400

    abi

--- abi.py
+++ abi.py
@@ -2,10 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+from bs4 import BeautifulSoup
+driver = webdriver.Firefox(executable_path='/home/quanabi/Desktop/abi/geckodriver')
+#driver.get("http://www.bzzii.glitch.me/")
+driver.get('https://profitcentr.com/')
+#bq = driver.page_source
+'''
+soup = BeautifulSoup(bq, 'html.parser')
+bq2 = soup.find_all('div',{'class': 'lRu31'})
+print(bq2)
+'''
+#/html/body/div[6]/table[2]/tbody/tr/td[1]/div/a[2]
+login =  driver.find_element(By.XPATH, "/html/body/div[6]/table[2]/tbody/tr/td[1]/div/a[2]")
+login.click()
+driver.close()
