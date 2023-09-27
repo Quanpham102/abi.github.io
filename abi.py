bb6f01096f927a598ec3ea4763138d4932e143f0
abiq02 <quanabi@debian.com>
2023-09-27 05:43:20 -0400

    abi

--- abi.py
+++ abi.py
@@ -14,30 +14,36 @@ time.sleep(10)
 aBi = driver.find_element(By.XPATH, "/html/body/div[6]/table[2]/tbody/tr/td[1]/div/a[2]")
 aBi.click() #login
 time.sleep(10)
-username = driver.find_element(By.NAME,'username')
-username.send_keys('heliiooo0202@gmail.com')
-time.sleep(2)
-password = driver.find_element(By.NAME,'password')
-password.send_keys('swml6c5d')
+
 abi = driver.page_source
 abi = re.findall('\n(.*Отметьте.*)\n',abi)[0]  
 print(abi)
 ru = abi
 bq = requests.get('https://translate.googleapis.com/translate_a/single?client=gtx&sl=ru&tl=vi&dt=t&q='+ru)
 print (bq.text)
-a = input('...')
-abi = driver.find_element(By.ID,'mnu_title1')
-time.sleep(10)
-abi.click()
-abi =  driver.find_element(By.XPATH,'/html/body/div[9]/table[2]/tbody/tr/td[1]/div/div[2]/div/center/div[1]/a[1]')
+username = driver.find_element(By.NAME,'username')
+username.send_keys('heliiooo0202@gmail.com')
+time.sleep(2)
+password = driver.find_element(By.NAME,'password')
+password.send_keys('swml6c5d')
+
+
+a = input('abi ...')
+abi1 = driver.find_element(By.XPATH,'//*[@id="mnu_title1"]')
+print(abi1)
+time.sleep(20)
+abi1.click()
+ab2i =  driver.find_element(By.XPATH,'/html/body/div[9]/table[2]/tbody/tr/td[1]/div/div[2]/div/center/div[1]/a[1]')
 time.sleep(5)
-abi.click()
+ab2i.click()
 driver.find_element(By.XPATH,'/html/body/div[9]/table[2]/tbody/tr/td[2]/div/div/div/div[1]/div[1]/table/tbody/tr/td[2]')#CLASS_NAME,'
 '''
+lass="usermnutitle-g"> is not clickable at point (102,452) because another element <html> obscures it
+
 heliiooo0202@gmail.com
 swml6c5d
 git pass
-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
+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
 bq = driver.page_source
 abi = open('a22.txt', 'w')
 abi.write(bq)
