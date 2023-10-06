67ccec5cb0869b813648e346e9bf2bbb21f85e91
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-10-06 18:32:29 +0700

    Update abi.py

--- abi.py
+++ abi.py
@@ -1,10 +1,1 @@
-chat_id ='5831792569'
-src = '/sdcard/Download/HD-wallpaper-tokyo-ghoul-kaneki-kaneki-ken-tokio-tokyo-ghoul-thumbnail.jpg'
-def sendPhoto(src,chat_id, caption):
-  url = "https://api.telegram.org/bot6691394722:AAEz0yMN644dj3Qp4GAsmKSuqmy0UWwNidQ/sendPhoto"
-  
-  image = open(src,'rb')
-  files =  {'photo' : image}
-  data ={'chat_id': chat_id,'caption' : caption}
-  response = requests.post(url, data = data, files= files)
-  
+
