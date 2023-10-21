dac01e9610f18995e98418f67dc8e02d804ac47e
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-10-21 17:16:35 +0700

    Update abi.py

--- abi.py
+++ abi.py
@@ -1,13 +1,36 @@
-from tkinter import *
+from tkinter import Tk, Label, Button, Entry, X
 
-#Tạo một cửa sổ mới
-window = Tk()
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
 
-#Thêm tiêu đề cho cửa sổ
-window.title('Welcome to VniTeach app')
 
-#Đặt kích thước của cửa sổ
-window.geometry('350x200')
+class Root(Tk):
+    def __init__(self):
+        super().__init__()
 
-#Lặp vô tận để hiển thị cửa sổ
-window.mainloop()
+        for x in [ttk.Label, ttk.Button, ttk.Entry, ttk.Checkbutton, ttk.Radiobutton]:
+            x(self, text='Some ' + x.__name__).pack(fill=X)
+        pb = ttk.Progressbar(self, length=100)
+        pb.pack(fill=X)
+        pb.step(33)
+
+        ttk.Label(self, text='Select theme:').pack(pady=[50, 10])
+
+        self.style = ttk.Style()
+        self.combo = ttk.Combobox(self, values=self.style.theme_names())
+        self.combo.pack(pady=[0, 10])
+        button = ttk.Button(self, text='Apply')
+        button['command'] = self.change_theme
+        button.pack(pady=10)
+
+    def change_theme(self):
+        content = self.combo.get()
+        try:
+            self.style.theme_use(content)
+        except:
+            print("Failed to set theme " + content)
+
+
+root = Root()
+root.mainloop()
